7ee2b34d0c586fe322f3700e52605b10f138dedd
Withered Flowers <withered-flowers@users.noreply.github.com>
2023-03-25 11:10:23 +0000

    feat: modify code for serverless

--- api/index.js
+++ api/index.js
@@ -7,7 +7,8 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 10000;
+// TODO: Comment kode di bawah ini
+// const port = process.env.PORT || 10000;
 
 const { User } = require("./models/index.js");
 
@@ -29,4 +30,7 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.listen(port, () => console.log(`Application is working at port ${port}`));
+// TODO: Ganti kode  di bawah ini
+
+// app.listen(port, () => console.log(`Application is working at port ${port}`));
+module.exports = app;
